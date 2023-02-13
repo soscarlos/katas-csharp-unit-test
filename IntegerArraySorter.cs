e65f5283dd4ad668567544fe2b8b3adbb1c445f7
soscarlos <soscarlos@gmail.com>
2023-02-13 09:39:42 +0100

    first commit for katas-unit test projects

---
+++ IntegerArraySorter.cs
@@ -0,0 +1,26 @@
+﻿namespace KatasCSharp;
+
+public class IntegerArraySorter
+{
+    public int[] SortArray(int[] array)
+    {
+       var oddNumbersQueue = GetOddNumbers(array);
+
+       for (var index = 0; index < array.Length; index++)
+       {
+           if (array[index] % 2 != 0)
+           {
+               array[index] = oddNumbersQueue.Dequeue();
+           }
+       }
+       return array;
+    }
+
+    private Queue<int> GetOddNumbers(int[] array)
+    {
+        return new Queue<int>(Array
+            .FindAll(array, number => number % 2 != 0)
+            .Order()
+            .ToArray());
+    }
+}
---
+++ IntegerArraySorterTest.cs
@@ -0,0 +1,15 @@
+﻿namespace KatasCSharp;
+
+public class IntegerArraySorterTest
+{
+    private readonly IntegerArraySorter _sorter = new IntegerArraySorter();
+
+    [TestCase(new int[] { 1, 3, 2, 8, 5, 4 }, new int[] { 5, 3, 2, 8, 1, 4 })]
+    [TestCase(new int[] { 1, 3, 5, 8, 0 }, new int[] { 5, 3, 1, 8, 0 })]
+    [TestCase(new int[] {}, new int[] {})]
+    public void BaseTest(int[] expected, int[] inputArray)
+    {
+        var actual = _sorter.SortArray(inputArray);
+        Assert.That(expected, Is.EqualTo(actual));
+    }
+}
